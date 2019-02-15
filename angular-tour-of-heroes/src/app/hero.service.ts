ace3278ee6e02a68ad29067c7ef2738a3e662a1f
henriquenovais <33753859+henriquenovais@users.noreply.github.com>
2019-02-15 18:27:01 -0300

    Remodel application for for Observable implementation

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Hero } from "./models/hero";
 import { HEROES } from "./models/mock-heroes";
-//import { Observable, of } from "rxjs";
+import { Observable, of } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
   constructor() {}
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -17,15 +17,27 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
+  /*
+  //This is an example of a synchronous(foreground) requisition to database. Which is 
+  //not a good idea since the server will have to handle the requests one by one
+  //while the application waits.
+  //This kind of requisition works here because the amount of data is saved in a local
+  //environment and the amount of data is very small
+
   getHeroes(): void {
     this.heroes = this.heroService.getHeroes();
   }
-
+  //In order to turn this synchronous request into an asynchronous request it is necessary 
+  //to make a Promise, a callback or an Observable. In this example we will use an observable.
+  */
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+  }
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     //Good place to put initialization logic
     //Angular calls this function shortly after creating a component
-    this.getHeroes();
+    this.getHeroes(); //Application calls the Hero database as soon as it starts successfully
   }
 }
